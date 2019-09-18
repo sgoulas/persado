b2fabd13a1519e7560a8bf73caef007d9fcfe12a
sgoulas <sprgoulas@gmail.com>
2019-09-19 01:04:55 +0300

    :bulb: changes the text of the index page to a more meaningful one

--- HTML/index.php
+++ HTML/index.php
@@ -79,7 +79,7 @@
     </div>
     <div class="col-sm-8 text-left"> 
       <h1>Welcome</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p>This is my technical assignment as part of my interview process for Persado.</p>
       <hr>
       <h3>Test</h3>
       <p>Lorem ipsum...</p>
@@ -96,7 +96,7 @@
 </div>
 
 <footer class="container-fluid text-center">
-  <p>Footer Text</p>
+  <p>sgoulas technical assignment for Persado interview process</p>
 </footer>
 
 </body>
