184d1b8b8522ef1d662ed28ba7c9f94a95bdf7b9
sgoulas <sprgoulas@gmail.com>
2019-09-18 23:46:24 +0300

    :bulb: adds Javascript folder structure and two class js files: Book and User

---
+++ JavaScript/Classes/Book.js
@@ -0,0 +1,42 @@
+class Book {
+    constructor(info) {
+        let _name = info.name;
+        let _summary = info.summary;
+        let _ISBN = info.ISBN;
+        let _available = info.available;
+        let _loaned = info.loaned;
+        let _id = info.hasOwnProperty("id") ? info.id : "-1";
+
+        this.getName = () => {
+            return _name;
+        };
+        this.getSummary = () => {
+            return _summary;
+        };
+        this.getISBN = () => {
+            return _ISBN;
+        };
+        this.getAvailable = () => {
+            return _available;
+        };
+        this.getLoaned = () => {
+            return _loaned;
+        };
+        this.getID = () => {
+            return _id;
+        };
+
+        this.toJSON = () => {
+            let json = {
+                name: _name,
+                summary: _summary,
+                ISBN: _ISBN,
+                available: _available,
+                loaned: _loaned
+                id: _id
+            };
+
+            return json;
+        };
+    }
+}
---
+++ JavaScript/Classes/User.js
@@ -0,0 +1,37 @@
+class User {
+    constructor(info) {
+        let _firstName = info.firstName;
+        let _lastName = info.lastName;
+        let _address = info.address;
+        let _booksLoaned = info.booksLoaned;
+        let _id = info.hasOwnProperty("id") ? info.id : "-1";
+
+        this.getFirstName = () => {
+            return _firstName;
+        };
+        this.getLastName = () => {
+            return _lastName;
+        };
+        this.getAddress = () => {
+            return _address;
+        };
+        this.getBooksLoaned = () => {
+            return _booksLoaned;
+        };
+        this.getID = () => {
+            return _id;
+        };
+
+        this.toJSON = () => {
+            let json = {
+                firstName: _firstName,
+                lastName: _lastName,
+                address: _address,
+                booksLoaned: _booksLoaned,
+                id: _id
+            };
+
+            return json;
+        };
+    }
+}
