fdece41e84eb5e5d10e36288f660879f63670232
sgoulas <sprgoulas@gmail.com>
2019-09-19 01:39:12 +0300

    :hammer: refactors the comments of the objectifySerializedArray function

--- JavaScript/Utils/utils.js
+++ JavaScript/Utils/utils.js
@@ -1,7 +1,9 @@
 /**
- * Get the result of a serializeArray() function call on a form
- * and return it as a valid JSON
+ * @param dataArray of type {Array}
+ * Gets the result of a array which is the result of a serializeArray() function call on a form
+ * @returns it as a valid JSON
  */
+
 const objectifySerializedArray = dataArray => {
     let json = {};
     for (let i = 0; i < dataArray.length; i++) {
