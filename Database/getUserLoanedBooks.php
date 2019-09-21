61ee455c16413afaf3ca7a65dde4e52b9f605f1b
sgoulas <sprgoulas@gmail.com>
2019-09-21 12:38:21 +0300

    :bulb: creates php file for retrieving loaned books based on user id

---
+++ Database/getUserLoanedBooks.php
@@ -0,0 +1,46 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    /*Get JSON as a string*/
+    $json_str = file_get_contents('php://input');
+    /*Get as an object*/
+    $json_obj = json_decode($json_str, false); //second parameter returns array instead of objects
+    
+    $userID   = $json_obj->ID;
+
+    $con = mysqli_connect("localhost", "root", "", "library");
+    
+    if (!$con) {
+        echo "error: to connect to MySQL: " . mysqli_connect_error();
+        exit();
+        
+    }
+    
+    mysqli_query($con, "SET NAMES 'utf8'");
+    mysqli_query($con, "SET CHARACTER SET 'utf8'");
+    
+    $getuserLoanedBooksQuery = "SELECT Books.Name FROM Registry INNER JOIN BOOKS 
+    ON Registry.Book_ID = Books.Book_ID 
+    WHERE U_ID='" . $userID . "' 
+    AND DateReturned IS NULL";
+
+    $result = mysqli_query($con, $getuserLoanedBooksQuery);
+    $rows = array();
+
+    if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+         $rows[] = $row;
+    }
+    print json_encode($rows);
+} else {
+    echo "0 results";
+}
+
+    
+    mysqli_close($con);
+    exit();
+} else {
+    echo "error: this is not a GET request";
+    exit();
+}
+?>
